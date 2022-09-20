927205fad23e1a451fa3cf00570ce8172e87e918
SAMUEL MARCIANO <samuel.marciano@senacsp.edu.br>
2022-09-19 22:19:02 -0300

    aula 1 de APP mobile

--- App.tsx
+++ App.tsx
@@ -1,20 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import MeuComponent from './MeuComponent';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <Text style={styles.title} >Meu App</Text>
+      <ScrollView>
+      <MeuComponent/>
+      <MeuComponent/>
+      <MeuComponent/>
+      <MeuComponent/>
+      <MeuComponent/>
+      <MeuComponent/>
+      <MeuComponent/>
+      <MeuComponent/>
+      <MeuComponent/>
+      <MeuComponent/>
+      <MeuComponent/>
+      <MeuComponent/>
+      <MeuComponent/>
+      <MeuComponent/>
+      </ScrollView>
     </View>
+    
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2a2a2a',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title:{
+    color:'#fff',
+    fontSize:24,
+    marginBottom:24,
+  },
+  card:{
+    backgroundColor:'#fff',
+    padding: 12,
+    borderRadius:3,
+  },
+  cardTitle:{
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  cardSubtitle:{
+    color:'#333',
+    fontSize:14
+  }
 });
